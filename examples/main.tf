d1b26c2f9c236c0c71b130d02c5dd03e25053a1c
Fabio Cicerchia <info@fabiocicerchia.it>
2026-01-24 14:18:24 +0100

    linted

--- examples/main.tf
+++ examples/main.tf
@@ -1,8 +1,12 @@
+terraform {
+  required_version = ">= 1.0"
+}
+
 module "carbon_intensity_exporter" {
   source = "../"
 
-  release_name    = var.release_name
-  namespace       = var.namespace
-  chart_version   = var.chart_version
-  values          = var.values
+  release_name  = var.release_name
+  namespace     = var.namespace
+  chart_version = var.chart_version
+  values        = var.values
 }
